7c455fa48e397aa1e3d8075a89bcf1b45669acb8
Yermolenko Dmitri <41588734+yermolenko-d@users.noreply.github.com>
2021-03-10 17:27:39 +0200

    added default route

---
+++ Abecado-Backend/Controllers/HomeController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Abecado_Backend.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class HomeController : ControllerBase
+    {
+        [HttpGet]
+        public string[] Index()
+        {
+            return new string[] { "Eto", "Noviy", "Project" };
+        }
+    }
+}
